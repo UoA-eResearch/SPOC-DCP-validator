c179bb1ca8783c256fc8d487cbb8ca191f3f73bb
Mike Laverick <mike.laverick@auckland.ac.nz>
2021-06-04 10:00:00 +1200

    initial upload of first working version

---
+++ gui/output.py
@@ -0,0 +1,88 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file './gui/DCP_validation.ui'
+#
+# Created by: PyQt5 UI code generator 5.9.2
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(880, 672)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.verticalLayout = QtWidgets.QVBoxLayout(self.centralwidget)
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.GroupBox = QtWidgets.QGroupBox(self.centralwidget)
+        self.GroupBox.setObjectName("GroupBox")
+        self.verticalLayout_2 = QtWidgets.QVBoxLayout(self.GroupBox)
+        self.verticalLayout_2.setContentsMargins(0, 0, 0, 0)
+        self.verticalLayout_2.setSpacing(2)
+        self.verticalLayout_2.setObjectName("verticalLayout_2")
+        self.plainTextEdit = QtWidgets.QPlainTextEdit(self.GroupBox)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.MinimumExpanding, QtWidgets.QSizePolicy.MinimumExpanding)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.plainTextEdit.sizePolicy().hasHeightForWidth())
+        self.plainTextEdit.setSizePolicy(sizePolicy)
+        self.plainTextEdit.setVerticalScrollBarPolicy(QtCore.Qt.ScrollBarAsNeeded)
+        self.plainTextEdit.setLineWrapMode(QtWidgets.QPlainTextEdit.NoWrap)
+        self.plainTextEdit.setObjectName("plainTextEdit")
+        self.verticalLayout_2.addWidget(self.plainTextEdit)
+        self.verticalLayout.addWidget(self.GroupBox)
+        self.groupBox = QtWidgets.QGroupBox(self.centralwidget)
+        self.groupBox.setObjectName("groupBox")
+        self.verticalLayout_3 = QtWidgets.QVBoxLayout(self.groupBox)
+        self.verticalLayout_3.setContentsMargins(0, 0, 0, 0)
+        self.verticalLayout_3.setObjectName("verticalLayout_3")
+        self.tableView = QtWidgets.QTableView(self.groupBox)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.MinimumExpanding, QtWidgets.QSizePolicy.Minimum)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.tableView.sizePolicy().hasHeightForWidth())
+        self.tableView.setSizePolicy(sizePolicy)
+        self.tableView.setMinimumSize(QtCore.QSize(0, 50))
+        self.tableView.setMaximumSize(QtCore.QSize(16777215, 150))
+        self.tableView.setAlternatingRowColors(False)
+        self.tableView.setWordWrap(False)
+        self.tableView.setObjectName("tableView")
+        self.tableView.horizontalHeader().setVisible(False)
+        self.tableView.horizontalHeader().setStretchLastSection(True)
+        self.tableView.verticalHeader().setVisible(False)
+        self.verticalLayout_3.addWidget(self.tableView)
+        self.verticalLayout.addWidget(self.groupBox)
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setObjectName("pushButton")
+        self.verticalLayout.addWidget(self.pushButton)
+        spacerItem = QtWidgets.QSpacerItem(0, 4, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Fixed)
+        self.verticalLayout.addItem(spacerItem)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 880, 26))
+        self.menubar.setObjectName("menubar")
+        self.menuFile = QtWidgets.QMenu(self.menubar)
+        self.menuFile.setObjectName("menuFile")
+        MainWindow.setMenuBar(self.menubar)
+        self.actionload = QtWidgets.QAction(MainWindow)
+        self.actionload.setObjectName("actionload")
+        self.actionsave = QtWidgets.QAction(MainWindow)
+        self.actionsave.setObjectName("actionsave")
+        self.menuFile.addAction(self.actionload)
+        self.menuFile.addAction(self.actionsave)
+        self.menubar.addAction(self.menuFile.menuAction())
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.GroupBox.setTitle(_translate("MainWindow", "Text Editor"))
+        self.groupBox.setTitle(_translate("MainWindow", "Validation Information"))
+        self.pushButton.setText(_translate("MainWindow", "Validate"))
+        self.menuFile.setTitle(_translate("MainWindow", "File"))
+        self.actionload.setText(_translate("MainWindow", "Load DCP..."))
+        self.actionsave.setText(_translate("MainWindow", "Save DCP..."))
---
+++ main.py
@@ -0,0 +1,276 @@
+import sys
+from PyQt5 import QtCore
+from PyQt5.QtWidgets import (QApplication, QMainWindow, QFileDialog,
+                             QMessageBox, QShortcut)
+from PyQt5.QtGui import QKeySequence, QTextBlockFormat, QTextCursor, QStandardItemModel, QStandardItem
+
+from gui.output import Ui_MainWindow
+
+
+def boolify(s):
+    if s == 'True' or s == 'true':
+            return True
+    if s == 'False' or s == 'false':
+            return False
+    raise ValueError('Not Boolean Value!')
+
+def estimateType(var):
+    '''guesses the str representation of the variables type'''
+    var = str(var) #important if the parameters aren't strings...
+    for caster in (boolify, int, float):
+            try:
+                    return caster(var)
+            except ValueError:
+                    pass
+    return var
+
+
+class MainWindow(QMainWindow, Ui_MainWindow):
+
+    def __init__(self, parent=None):
+        # Initialise the Main window
+        QMainWindow.__init__(self, parent=parent)
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+        self.ui.actionload.triggered.connect(lambda: self.load_data())
+        self.ui.actionsave.triggered.connect(lambda: self.save_data())
+
+        self.save_shortcut = QShortcut(QKeySequence("Ctrl+S"), self.ui.plainTextEdit)
+        self.save_shortcut.activated.connect(lambda: self.save_data())
+
+        self.ui.pushButton.clicked.connect(lambda: self.validate())
+        #self.ui.StopButton.clicked.connect(lambda: self.stop_exit())
+
+        self.highlight_format = QTextBlockFormat()
+        self.highlight_format.setBackground(QtCore.Qt.yellow)
+        self.error_format = QTextBlockFormat()
+        self.error_format.setBackground(QtCore.Qt.red)
+        self.default_format = QTextBlockFormat()
+        self.default_format.setBackground(QtCore.Qt.white)
+        self.validate_info = {
+            "version":{
+                "mandatory":True, "present":False, "unique":True,
+                "args":[int(), int(), int()]
+                       },
+
+            "command_timeout":{
+                "mandatory":True, "present":False, "unique":True,
+                "args":[int()]
+                       },
+            "init":{
+                "mandatory":True, "present":False, "unique":True,
+                "args":[str()]
+                       },
+            "ascending":{
+                "mandatory":True, "present":False, "unique":True,
+                "args":[str(), int()]
+                       },
+            "descending":{
+                "mandatory":True, "present":False, "unique":True,
+                "args":[str(), int()]
+                       },
+            "exit":{
+                "mandatory":True, "present":False, "unique":True,
+                "args":[str()]
+                       },
+            "region":{
+                # args for region is special: nested lists of types per expected line
+                "mandatory":False, "present":False, "unique":False,
+                "args":[[str()],
+                        [str()],
+                        [float(), float()],
+                        [float(), float()],
+                        [float(), float()],
+                        [float(), float()]]
+                       },
+        }
+
+        self.tablemodel = QStandardItemModel(self)
+        self.ui.tableView.setModel(self.tablemodel)
+
+    def load_data(self):
+        self.tablemodel.clear()
+        try:
+            data_path, _ =QFileDialog.getOpenFileName(None, 'Open File', "./DCP_files/", '*.txt')
+            dcp_file = open(data_path, "r")
+            self.ui.plainTextEdit.clear()
+            for line in dcp_file.readlines():
+                self.ui.plainTextEdit.appendPlainText(line.rstrip('\n')) #strip newlines from end
+            self.ui.plainTextEdit.document().setModified(False)
+            #print(self.ui.plainTextEdit.document().isModified())
+        except FileNotFoundError:
+            pass
+        except Exception as e:
+            msg = QMessageBox()
+            msg.setIcon(QMessageBox.Critical)
+            msg.setText(str(type(e)))
+            msg.setInformativeText(str(e))
+            msg.setWindowTitle("Error")
+            msg.exec_()
+
+
+    def save_data(self):
+        try:
+            data_path, _ =QFileDialog.getSaveFileName(None, 'Save File', "./DCP_files/", '*.txt')
+            with open(data_path, "w") as save_file:
+                for line in range(self.ui.plainTextEdit.document().blockCount()):
+                    save_file.write(self.ui.plainTextEdit.document().findBlockByLineNumber(line).text()+str("\n"))
+                save_file.close()
+        except FileNotFoundError:
+            pass
+        except Exception as e:
+            msg = QMessageBox()
+            msg.setIcon(QMessageBox.Critical)
+            msg.setText(str(type(e)))
+            msg.setInformativeText(str(e))
+            msg.setWindowTitle("Error")
+            msg.exec_()
+
+
+    def validate(self):
+        self.tablemodel.clear()
+        try:
+            # this will go as high as 6 before rolling back to 0
+            region_counter = 0
+            for line in range(self.ui.plainTextEdit.document().blockCount()):
+                line_obj = self.ui.plainTextEdit.document().findBlockByLineNumber(line)
+
+                line_text = line_obj.text().split('#',1)[0]
+                #print(line_text)
+                recognised_command = False
+                line_breakdown = line_text.strip().split(" ")
+                line_breakdown = [estimateType(x) for x in line_breakdown]
+
+                # if "region block" validation is activated
+                if region_counter:
+                    # check to make sure lines are indented
+                    if len(line_text.rstrip().split(" ")) == len(line_breakdown):
+                        cursor = QTextCursor(line_obj)
+                        cursor.setBlockFormat(self.highlight_format)
+                        self.tablemodel.appendRow(QStandardItem("Line "+str(line)+ ": Indent missing - part of \"region\" block"))
+
+                    # check if number of arguments are correct
+                    elif len(line_breakdown) != len(self.validate_info["region"]["args"][region_counter-1]):
+                        cursor = QTextCursor(line_obj)
+                        cursor.setBlockFormat(self.highlight_format)
+                        self.tablemodel.appendRow(QStandardItem("Line "+str(line)+ ': Incorrect number of arguments - expected ('+
+                                                                str(len(self.validate_info["region"]["args"][region_counter-1])) +
+                                                                ") but got ("+str(len(line_breakdown)) +")CORRECT FOR SPACES"))
+                    # check if arguments types are correct
+                    elif [type(x) for x in line_breakdown] != [type(y) for y in self.validate_info["region"]["args"][region_counter-1]]:
+                        cursor = QTextCursor(line_obj)
+                        cursor.setBlockFormat(self.highlight_format)
+                        self.tablemodel.appendRow(QStandardItem("Line "+str(line)+ ': Incorrect argument types - expected ('+
+                                                                str("INSERT HERE") +
+                                                                ") but got ("+str("INSERT HERE") +")"))
+                    # line seems valid, remove any highlights
+                    else:
+                        cursor = QTextCursor(line_obj)
+                        cursor.setBlockFormat(self.default_format)
+                    if region_counter > 5:
+                        region_counter = 0
+                    else:
+                        region_counter += 1
+                    continue
+
+                for key, value in self.validate_info.items():
+                    if key in line_breakdown:
+                        # Command is approved
+                        recognised_command = True
+                        # if key already in DCP - raise multiple error
+                        if self.validate_info[key]["present"] and self.validate_info[key]["unique"]:
+                            cursor = QTextCursor(line_obj)
+                            cursor.setBlockFormat(self.error_format)
+                            msg = QMessageBox()
+                            msg.setIcon(QMessageBox.Critical)
+                            msg.setText("Too many occurences of \""+str(key)+"\" in file")
+                            msg.setInformativeText("The \""+str(key)+"\" command should only be specified once")
+                            msg.setWindowTitle("Too many occurences of \""+str(key)+"\" in file")
+                            msg.exec_()
+                            self.tablemodel.appendRow(QStandardItem("Line "+str(line)+ ": \""+str(key)+"\" command is already present"))
+                            break
+                        # Flag the command as present in the editor: Do this after "already-present" check
+                        self.validate_info[key]["present"] = True
+
+                        # check if first arguments is the command in question
+                        if line_breakdown[0] != key:
+                            cursor = QTextCursor(line_obj)
+                            cursor.setBlockFormat(self.highlight_format)
+                            self.tablemodel.appendRow(QStandardItem("Line "+str(line)+ ": \""+str(key)+"\" command must occur at start of line"))
+
+                            break
+                        #this is special, activate region_counter and break
+                        elif key == "region":
+                            region_counter += 1
+                            break
+                        # check if number of arguments are correct
+                        elif len(line_breakdown) != len(value["args"])+1:
+                            cursor = QTextCursor(line_obj)
+                            cursor.setBlockFormat(self.highlight_format)
+                            self.tablemodel.appendRow(QStandardItem("Line "+str(line)+ ': Incorrect number of arguments - expected ('+
+                                                                    str(len(self.validate_info["region"]["args"][region_counter-1])) +
+                                                                    ") but got ("+str(len(line_breakdown)) +")CORRECT FOR SPACES"))
+                            break
+                        # check if arguments types are correct
+                        elif [type(x) for x in line_breakdown[1:]] != [type(y) for y in value["args"]]:
+                            cursor = QTextCursor(line_obj)
+                            cursor.setBlockFormat(self.highlight_format)
+                            self.tablemodel.appendRow(QStandardItem("Line "+str(line)+ ': Incorrect argument types - expected ('+
+                                                                    str("INSERT HERE") +
+                                                                    ") but got ("+str("INSERT HERE") +")"))
+                            break
+                        # line seems valid, remove any highlights
+                        else:
+                            cursor = QTextCursor(line_obj)
+                            cursor.setBlockFormat(self.default_format)
+                            break
+
+                # "command" on line is not recognised
+                if not recognised_command:
+                    # actual input on line, highlight as unknown command
+                    if line_breakdown[0]:
+                        cursor = QTextCursor(line_obj)
+                        cursor.setBlockFormat(self.highlight_format)
+                        self.tablemodel.appendRow(QStandardItem("Line "+str(line)+ ": Unrecognised command"))
+                    # don't worry about "empty" lines
+                    else:
+                        cursor = QTextCursor(line_obj)
+                        cursor.setBlockFormat(self.default_format)
+
+            # make sure all mandatory fields are present, if not then raise error message
+            mandatory_fields = [(key, value["present"]) for (key, value) in self.validate_info.items() if value["mandatory"] == True]
+            if not all([tup[1] for tup in mandatory_fields]):
+                missing_fields = [key for (key, value) in mandatory_fields if value == False]
+                #print("False detected in following fields: " + ", ".join(missing_fields))
+                msg = QMessageBox()
+                msg.setIcon(QMessageBox.Critical)
+                msg.setText("Commands missing from the DCP file")
+                msg.setInformativeText("The following commands are missing from the DCP file: " + ", ".join(missing_fields))
+                msg.setWindowTitle("Commands missing from the DCP file")
+                msg.exec_()
+                for missing in missing_fields:
+                    self.tablemodel.appendRow(QStandardItem("Command missing from file: \""+str(missing)+"\""))
+        except FileNotFoundError:
+            pass
+        except Exception as e:
+            msg = QMessageBox()
+            msg.setIcon(QMessageBox.Critical)
+            msg.setText(str(type(e)))
+            msg.setInformativeText(str(e))
+            msg.setWindowTitle("Error")
+            msg.exec_()
+
+        for key, value in self.validate_info.items():
+            self.validate_info[key]["present"] = False
+        #self.tablemodel.appendRow(QStandardItem("BLAH BLAH"))
+        if not self.tablemodel.rowCount():
+            self.tablemodel.appendRow(QStandardItem("No errors found: DCP is valid!"))
+        self.ui.tableView.model().layoutChanged.emit()
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    w = MainWindow()
+    w.show()
+    sys.exit(app.exec_())
